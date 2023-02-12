b1133ade21ecf068c3379c36f4e9ec912fe6fd32
Allen Tan <allen@tanmade.com>
2023-02-12 16:15:27 -0500

    Update sectionDonate.js

--- sanity/schemas/sectionDonate.js
+++ sanity/schemas/sectionDonate.js
@@ -2,7 +2,7 @@ export default {
   name: 'sectionDonate',
   title: 'Section: Donate',
   type: 'document',
-  __experimental_actions: ['create', 'update', /*'delete',*/ 'publish'],
+  __experimental_actions: [/*'create',*/ 'update', /*'delete',*/ 'publish'],
   fields: [
     {
       name: 'title',
