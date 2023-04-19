0a7439ef5d02fb8fc0698f673f081a75e19b3810
Allen Tan <allen@tanmade.com>
2023-04-18 22:57:13 -0400

    Add close button link

--- components/Bio.js
+++ components/Bio.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 import BlockContent from '@sanity/block-content-to-react'
 
@@ -11,6 +12,9 @@ class Bio extends React.Component {
     return (
       <>
         <div className="bioWrapper">
+          <a className="closeButton" href="/">×</a>
+          <h1>{this.props.title}</h1>
+          <div className="bioLayout">
             <div>
               <img
                 className="bioImage"
@@ -19,36 +23,68 @@ class Bio extends React.Component {
               />
             </div>
             <div>
-                <h1>{this.props.title}</h1>
                 <BlockContent
                     blocks={this.props.body}
                     imageOptions={{ w: 750, fit: 'max' }}
                 />
             </div>
+          </div>
         </div>
         <style jsx>{`
           .bioWrapper {
+            position: relative;
             width: calc(100% - 30px);
             max-width: 1000px;
 
-            margin: 15px auto;
+            margin: 20px auto;
             padding: 20px;
 
             background: #fff;
             border-radius: 3px;
+          }
 
+          @media screen and (min-width: 720px) {
+            .bioWrapper {
+              margin: 60px auto;
+              padding: 60px 60px 60px 60px;
+            }
+          }
+
+          .bioLayout {
             display: flex;
             flex-direction: column;
             gap: 2rem;
           }
 
           @media screen and (min-width: 720px) {
-            .bioWrapper {
-              margin: 60px auto;
-              padding: 60px;
-
-              flex-direction: row;
+            .bioLayout {
+              flex-direction: row-reverse;
               gap: 3rem;
+            }
+          }
+
+          .closeButton {
+            display: inline-block; width: 2.5rem; height: 2.5rem; text-align: center; line-height: 1.25;
+            background: rgba(255,255,255,0.8); border-radius: 50%;
+
+            font-size: 2rem;
+            font-weight: 700;
+
+            float: right;
+            position: sticky;
+            top: 16px;
+          }
+
+          .closeButton:hover {
+            background: rgba(240,240,240,0.8);
+          }
+
+          @media screen and (min-width: 720px) {
+            .closeButton {
+              position: sticky;
+              top: 16px;
+              margin-top: -44px;
+              transform: translateX(3rem);
             }
           }
 
