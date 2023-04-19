9c0cd6e160c09de648dec4ebc5cc1010d6e6cdaf
Allen Tan <allen@tanmade.com>
2023-04-18 21:52:23 -0400

    Update bio layout

--- components/Bio.js
+++ components/Bio.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import Container from './Container'
-
 import BlockContent from '@sanity/block-content-to-react'
 
 class Bio extends React.Component {
@@ -12,14 +10,53 @@ class Bio extends React.Component {
   render() {
     return (
       <>
-        <Container>
-            <h1>{this.props.title}</h1>
-            <BlockContent
-                blocks={this.props.body}
-                imageOptions={{ w: 750, fit: 'max' }}
-            />
-        </Container>
+        <div className="bioWrapper">
+            <div>
+              <img
+                className="bioImage"
+                src="https://placehold.co/500x750/png"
+                alt={ "Picture of " + this.props.title }
+              />
+            </div>
+            <div>
+                <h1>{this.props.title}</h1>
+                <BlockContent
+                    blocks={this.props.body}
+                    imageOptions={{ w: 750, fit: 'max' }}
+                />
+            </div>
+        </div>
         <style jsx>{`
+          .bioWrapper {
+            width: calc(100% - 30px);
+            max-width: 1000px;
+
+            margin: 15px auto;
+            padding: 20px;
+
+            background: #fff;
+            border-radius: 3px;
+
+            display: flex;
+            flex-direction: column;
+            gap: 2rem;
+          }
+
+          @media screen and (min-width: 720px) {
+            .bioWrapper {
+              margin: 60px auto;
+              padding: 60px;
+
+              flex-direction: row;
+              gap: 3rem;
+            }
+          }
+
+          @media screen and (min-width: 720px) {
+            .bioImage {
+              position: sticky; top: 60px;
+            }
+          }
         `}</style>
       </>
     )
