0e0895704372e1d03ce17a162c98a94eda9f4a3e
Allen Tan <allen@tanmade.com>
2025-10-25 16:36:11 -0400

    Add copyright sign to try and trigger some other cache clearing

--- components/Footer.js
+++ components/Footer.js
@@ -14,7 +14,7 @@ class Footer extends React.Component {
         <Container>
           <Section padding="small" hasBorder>
             <footer>
-              <span>{new Date().getFullYear()}, Ensemble Signal</span>
+              <span>© {new Date().getFullYear()}, Ensemble Signal</span>
             </footer>
           </Section>
         </Container>
